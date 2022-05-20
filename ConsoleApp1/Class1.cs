55a3613ac789939831492893dbc7f2886995f131
Shree <Shree@DESKTOP-EB4S8S8>
2022-05-20 11:23:44 +0530

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[]array = { 10, 20, 30 ,40,50,60,70,80};
+            int sum = 0;
+            for(int i = 0; i < array.Length-1; i=+2)
+            {
+                sum= array[i] + array[i+1];
+                Console.WriteLine(sum);
+            }
+           
+        }
+    }
+}
---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace ConsoleApp1
+{
+     class Helloworld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
